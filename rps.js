b3cf4f8abfd064f6da23035582a44ac625e9c4d0
MfnBacon <83557762+MfnBacon@users.noreply.github.com>
2022-01-04 21:10:35 -0600

    Many Fixes
    Took quite a bit to figure out how to place all of this so it all worked without removing too much of the original "plan".

--- rps.js
+++ rps.js
@@ -1,62 +1,93 @@
-getElementById("rock").addEventListener("click", userSelection);
-getElementbyId("paper").addEventListener("click", userSelection);
-getElementbyId("scissors").addEventListener("click", userSelection);
+// Need these for later code
+var userPts = 0;
+var compPts = 0;
+var round = 0;
 
-    function computerPlay() { //computer generates a random answer.
-        let random = ["rock", "paper", "scissors"];
-        return random[Math.floor(Math.random() * 3)];
-        }
-        
-        function autoPlay(){ // Automates User for testing purposes
-            let random = ["rock", "paper", "scissors"];
-            return random [Math.floor(Math.random() * 3)];
-        }
-    function playRound(userSelection, computerSelection) { //plays a round of the game.
-        if (userSelection === computerSelection) {
-        return draw
-        }
-            else if
-                (userSelection === "rock" && computerSelection === "paper" ||
-                userSelection === "paper" && computerSelection === "scissors" ||
-                userSelection === "scissors" && computerSelection === "rock") { 
-                    compPts++
-                    return lose
-                }
-                    else {
-                        userPts++
-                        return win
-                    }
-        }
-        
-        
-        let userPts = parseInt(0);
-        let compPts = parseInt(0);
-        let win = "You win!"
-        let lose = "You lost!"
-        let draw = "It is a tie!"
-        
-            for(var i=0;i<5;i++){
-                let userSelection = prompt("Choose your weapon!");
-                const computerSelection = computerPlay()
-                console.log(playRound(userSelection, computerSelection));
-                console.log("Your score = " + userPts);
-                console.log("Computer's score = " + compPts);
-            }
-            /*
-                if (i === 5) {
-                if (userPts < compPts){
-                    console.log("You Lose! " + userPts + " to " + compPts + ". Starting Over in 3 Seconds");     
-                    setTimeout(() => {  location.reload(); }, 3000);
-                }
-                else if (compPts < userPts){    
-                    console.log("You Win! " + userPts + " to " + compPts + ". Starting Over in 3 Seconds");     
-                    setTimeout(() => {  location.reload(); }, 3000);
-                }
-                else {
-                    console.log("Tie Game! Starting Over in 3 Seconds");     
-                    setTimeout(() => {  location.reload(); }, 3000);
-                }
+document.getElementById("userPts").textContent = userPts;
+document.getElementById("compPts").textContent = compPts;
+document.getElementById("messageBox").textContent = 'First to Five Points. Lets do it.';
 
-       
+// Capitalizes first letter of return output.
+function capitalize(choice) {
+    return choice.charAt(0).toUpperCase() + choice.slice(1);
+}
+
+
+// Random Computer Selection
+function computerPlay() {
+    var compOptions = ['rock','paper','scissors'];
+    return compOptions[Math.floor(Math.random()*compOptions.length)];
+    
+}
+
+
+function playRound(userSelection, computerSelection) { 
+    var userWin = "You've Won! Computer Chose " + capitalize(computerSelection) + '.';
+    var userLose = "You've Lost! Computer Chose " + capitalize(computerSelection) + '.';
+    var draw = "It's a Draw!";
+    round++;
+    if (userSelection === computerSelection) {
+    return draw
     }
-    */
+        else if
+            (userSelection === "rock" && computerSelection === "paper" ||
+            userSelection === "paper" && computerSelection === "scissors" ||
+            userSelection === "scissors" && computerSelection === "rock") { 
+                compPts++
+                return userLose
+            }
+                else {
+                    userPts++
+                    return userWin
+                }
+    }
+    
+    function game(player) {
+        if(userPts < 5 && compPts < 5) {
+        var playerSelection = player
+        const computerSelection = computerPlay();
+        var roundText = playRound(playerSelection, computerSelection)
+        document.getElementById("messageBox").textContent = roundText;
+        document.getElementById("userPts").textContent = userPts;
+        document.getElementById("compPts").textContent = compPts;
+        
+    }
+        
+        if (userPts == 5) {
+            document.getElementById('messageBox').textContent = "Game Over! Click Restart.";
+        }
+        if (compPts == 5) {
+            document.getElementById('messageBox').textContent = "Game Over! Click Restart.";
+        }
+    }
+
+        var rockBtn = document.querySelector('#rockBtn');
+        var paperBtn = document.querySelector('#paperBtn');
+        var scissorBtn = document.querySelector('#scissorBtn');
+        
+        
+
+        rockBtn.addEventListener('click', ()=>  {
+            game('rock');
+            console.log("Chose Rock");
+        });
+        
+        paperBtn.addEventListener('click', ()=>  {
+            game('paper');
+            console.log("Chose Paper");
+        });
+        
+        scissorBtn.addEventListener('click', ()=>  {
+            game('scissors');
+            console.log("Chose Scissors");
+        });
+        
+        
+resetBtn.addEventListener('click', ()=>  {
+    userPts = 0;
+    compPts = 0;
+    document.getElementById("userPts").textContent = userPts;
+    document.getElementById("compPts").textContent = compPts;
+    document.getElementById("messageBox").textContent = 'First to Five Points. Lets do it.';
+
+});
